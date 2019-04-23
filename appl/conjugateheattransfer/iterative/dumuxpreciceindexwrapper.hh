63a632587db4f6f26cefca0990302c6bbbe0511a
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2019-04-23 17:12:36 +0200

    wrapper for bidirectional mapping of indices between DuMuX and preCICE

---
+++ appl/conjugateheattransfer/iterative/dumuxpreciceindexwrapper.hh
@@ -0,0 +1,11 @@
+#ifndef DUMUXPRECICEINDEXWRAPPER_H
+#define DUMUXPRECICEINDEXWRAPPER_H
+
+
+class DumuxPreciceIndexWrapper
+{
+  public:
+    DumuxPreciceIndexWrapper();
+};
+
+#endif // DUMUXPRECICEINDEXWRAPPER_H
