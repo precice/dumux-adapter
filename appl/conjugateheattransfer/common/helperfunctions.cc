0c73311be1f0c63d39ca37b135bae681c3166cfb
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2019-05-15 16:34:39 +0200

    slight cleanup; hopefully code compiles on other platforms now

---
+++ appl/conjugateheattransfer/common/helperfunctions.cc
@@ -0,0 +1,6 @@
+#include "helperfunctions.hh"
+
+helperfunctions::helperfunctions()
+{
+
+}
---
+++ appl/conjugateheattransfer/common/helperfunctions.hh
@@ -0,0 +1,11 @@
+#ifndef HELPERFUNCTIONS_HH
+#define HELPERFUNCTIONS_HH
+
+
+class helperfunctions
+{
+  public:
+    helperfunctions();
+};
+
+#endif // HELPERFUNCTIONS_HH
