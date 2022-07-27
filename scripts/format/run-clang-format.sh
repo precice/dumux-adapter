b84905b119564212759b522d22f01adf038bb852
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2022-07-27 16:53:11 +0200

    Fix CI and clang format

--- scripts/format/run-clang-format.sh
+++ scripts/format/run-clang-format.sh
@@ -3,9 +3,9 @@ set -e
 # Runs clang-format on all *.cc and *.hh files
 DIRECTORIES=("dumux-precice/" "examples/" "test/" "postprocessing/")
 for DIRECTORY in "${DIRECTORIES[@]}"; do
-    echo "Check files in ${DIRECTORY}"
+    #echo "Check files in ${DIRECTORY}"
     for FILE in $(find "${DIRECTORY}" -regex '.*\.\(cc\|hh\)' ); do
-        echo "$FILE"
+        #echo "$FILE"
         clang-format-10 --dry-run -style=file -Werror -i ${FILE}
     done
     if [ $? -ne 0 ]; then
