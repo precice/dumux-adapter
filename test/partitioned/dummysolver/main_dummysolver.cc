85b5670bbeb26a2751bee2493c9eed71c1e2d7c0
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2022-07-27 14:59:36 +0200

    Compiling version of solver dummy

--- test/partitioned/dummysolver/main_dummysolver.cc
+++ test/partitioned/dummysolver/main_dummysolver.cc
@@ -11,9 +11,12 @@
 
 #include <dune/common/parallel/mpihelper.hh>
 #include <dune/common/timer.hh>
+#include <dune/istl/io.hh>
+
+#include <dumux/common/dumuxmessage.hh>
+#include <dumux/common/parameters.hh>
 
 #include "dumux-precice/couplingadapter.hh"
-
 
 int main(int argc, char **argv)
 try {
@@ -29,9 +32,9 @@ try {
     // parse command line arguments and input file
     Parameters::init(argc, argv);
 
-    const std::string solverName = getParamFromGroup<std::string>"preCICE", "SolverName");
-    const std::string preciceConfigFilename =    getParamFromGroup<std::string>"preCICE", "ConfigFileName");
-    const std::string meshName =    getParamFromGroup<std::string>"preCICE", "MeshName");
+    const std::string solverName = getParamFromGroup<std::string>("preCICE", "SolverName");
+    const std::string preciceConfigFilename =    getParamFromGroup<std::string>("preCICE", "ConfigFileName");
+    const std::string meshName =    getParamFromGroup<std::string>("preCICE", "MeshName");
 
 
     // Initialize preCICE.Tell preCICE about:
@@ -45,92 +48,162 @@ try {
     //     preciceConfigFilename = argv[argc - 1];
 
     auto &couplingInterface = Dumux::Precice::CouplingAdapter::getInstance();
-    couplingInterface.announceSolver("FreeFlow", preciceConfigFilename,
+    couplingInterface.announceSolver(solverName, preciceConfigFilename,
                                      mpiHelper.rank(), mpiHelper.size());
 
-    const int dim = couplingInterface.getDimensions();
-    // std::cout << dim << "  " << int(FreeFlowGridGeometry::GridView::dimension)
-    //           << std::endl;
 
-    // GET mesh corodinates
-    std::vector<double> coords;  //( dim * vertexSize );
-    std::vector<int> coupledScvfIndices;
+    std::cout << "DUMMY (" << mpiHelper.rank()
+              << "): Running solver dummy with preCICE config file \"" << preciceConfigFilename
+              << "\", participant name \"" << solverName
+              << "\", and mesh name \"" << meshName
+              << "\".\n";
 
-    for (const auto &element : elements(freeFlowGridView)) {
-        auto fvGeometry = localView(*freeFlowGridGeometry);
-        fvGeometry.bindElement(element);
+    const int dimensions = couplingInterface.getDimensions();
+    assert( dimensions == 3 );
+    const std::string scalarDataWriteName = (solverName == "SolverOne") ? "scalarDataOne" : "scalarDataTwo" ;
+    const std::string scalarDataReadName = (solverName == "SolverOne") ? "scalarDataOne" : "scalarDataTwo" ;
 
-        for (const auto &scvf : scvfs(fvGeometry)) {
-            static constexpr auto eps = 1e-7;
-            const auto &pos = scvf.center();
-            if (pos[1] < freeFlowGridGeometry->bBoxMin()[1] + eps) {
-                if (pos[0] > xMin - eps && pos[0] < xMax + eps) {
-                    coupledScvfIndices.push_back(scvf.index());
-                    for (const auto p : pos)
-                        coords.push_back(p);
-                }
-            }
+    const std::string vectorDataWriteName = (solverName == "SolverOne") ? "vectorDataOne" : "vectorDataTwo" ;
+    const std::string vectorDataReadName = (solverName == "SolverOne") ? "vectorDataOne" : "vectorDataTwo" ;
+
+    const int numberOfVertices = 3;
+
+    std::vector<double> readScalarData(numberOfVertices);
+    std::vector<double> writeScalarData(numberOfVertices);
+    std::vector<double> readVectorData(numberOfVertices * dimensions);
+    std::vector<double> writeVectorData(numberOfVertices * dimensions);
+    std::vector<double> vertices(numberOfVertices * dimensions);
+    std::vector<int>    preciceVertexIDs(numberOfVertices);
+    std::vector<int>    dumuxVertexIDs(numberOfVertices);
+
+    for (int i = 0; i < numberOfVertices; i++) {
+        writeScalarData.at(i) = i + numberOfVertices;
+        dumuxVertexIDs.at(i) = i + numberOfVertices;
+        for (int j = 0; j < dimensions; j++) {
+            vertices.at(j + dimensions * i)  = i;
+            writeVectorData.at(j + dimensions * i) = i;
         }
     }
 
-    const auto numberOfPoints = coords.size() / dim;
-    const double preciceDt = couplingInterface.setMeshAndInitialize(
-        "FreeFlowMesh", numberOfPoints, coords);
-    couplingInterface.createIndexMapping(coupledScvfIndices);
+    double preciceDt = couplingInterface.setMeshAndInitialize(meshName, numberOfVertices, vertices);
 
-    const auto velocityId = couplingInterface.announceScalarQuantity("Velocity");
-    const auto pressureId = couplingInterface.announceScalarQuantity("Pressure");
+    // Create index mapping between DuMuX's index numbering and preCICE's numbering
+    couplingInterface.createIndexMapping(dumuxVertexIDs);
 
+    // const int readScalarDataID  = couplingInterface.getDataID(scalarDataReadName, meshID);
+    // const int writeScalarDataID = couplingInterface.getDataID(scalarDataWriteName, meshID);
+
+    // const int readVectorDataID  = couplingInterface.getDataID(vectorDataReadName, meshID);
+    // const int writeVectorDataID = couplingInterface.getDataID(vectorDataWriteName, meshID);
+
+    const int readScalarDataID  = couplingInterface.announceScalarQuantity(scalarDataReadName);
+    const int writeScalarDataID = couplingInterface.announceScalarQuantity(scalarDataWriteName);
+
+    const int readVectorDataID  = couplingInterface.announceVectorQuantity(vectorDataReadName);
+    const int writeVectorDataID = couplingInterface.announceVectorQuantity(vectorDataWriteName);
+
+    // const auto velocityId = couplingInterface.announceScalarQuantity("Velocity");
+    // const auto pressureId = couplingInterface.announceScalarQuantity("Pressure");
 
     if (couplingInterface.hasToWriteInitialData()) {
-        couplingInterface.writeScalarQuantityToOtherSolver(pressureId);
+        // Scalar data
+        couplingInterface.writeQuantityVector(writeScalarDataID, writeScalarData);
+        couplingInterface.writeScalarQuantityToOtherSolver(writeScalarDataID);
+        // Vector data
+        couplingInterface.writeQuantityVector(writeVectorDataID, writeVectorData);
+        couplingInterface.writeScalarQuantityToOtherSolver(writeVectorDataID);
         couplingInterface.announceInitialDataWritten();
     }
     couplingInterface.initializeData();
 
-    auto dt = preciceDt;
-    auto sol_checkpoint = sol;
+    // Check exchanged initial data
+    // {
+    //     for (int i = 0; i < numberOfVertices; i++) {
+    //         if ( readScalarData.at(i) != writeScalarData.at(i) ) {
+    //             std::cout << "DUMMY (" << mpiHelper.rank()
+    //                       << "): Reading initialized SCALAR data error\n"
+    //                       << "Expected " << writeScalarData.at(i)
+    //                       << ", Found " << readScalarData.at(i)
+    //                       << "\n";
+    //         }
 
-    double vtkTime = 1.0;
-    size_t iter = 0;
+    //         for (int j = 0; j < dimensions; j++) {
+    //             if ( readVectorData.at(j + dimensions * i) != writeVectorData.at(j + dimensions * i) ) {
+    //                 std::cout << "DUMMY (" << mpiHelper.rank()
+    //                           << "): Reading initialized VECTOR data error\n"
+    //                           << "Expected " << writeVectorData.at(j + dimensions * i)
+    //                           << ", Found " << readVectorData.at(j + dimensions * i)
+    //                           << "\n";
+    //             }
+    //         }
+    //     }
+    // }
+    std::fill( readScalarData.begin(), readScalarData.end(), 0 );
+    std::fill( readVectorData.begin(), readVectorData.end(), 0 );
+
+    int iter = 0;
 
     while (couplingInterface.isCouplingOngoing()) {
         if (couplingInterface.hasToWriteIterationCheckpoint()) {
-            //DO CHECKPOINTING
-            sol_checkpoint = sol;
+            std::cout << "DUMMY (" << mpiHelper.rank() << "): Writing iteration checkpoint\n";
             couplingInterface.announceIterationCheckpointWritten();
         }
 
-        couplingInterface.readScalarQuantityFromOtherSolver(velocityId);
-        // solve the non-linear system
-        // Nothing to do in dummy
+        //Read data
+        couplingInterface.readQuantityFromOtherSolver( readScalarDataID, Dumux::Precice::QuantityType::Scalar );
+        couplingInterface.readQuantityFromOtherSolver( readVectorDataID, Dumux::Precice::QuantityType::Vector );
 
-        // TODO
-        setInterfacePressures<FluxVariables>(*freeFlowProblem,
-                                             *freeFlowGridVariables, sol);
-        couplingInterface.writeScalarQuantityToOtherSolver(pressureId);
+        // Check data
+        for (int i = 0; i < numberOfVertices; i++) {
+            if ( readScalarData.at(i) != writeScalarData.at(i) ) {
+                std::cout << "DUMMY (" << mpiHelper.rank()
+                          << "): Reading initialized SCALAR data error\n"
+                          << "Expected " << writeScalarData.at(i)
+                          << ", Found " << readScalarData.at(i)
+                          << "\n";
+            }
 
-        //Read checkpoint
-        freeFlowVtkWriter.write(vtkTime);
-        vtkTime += 1.;
-        const double preciceDt = couplingInterface.advance(dt);
-        dt = std::min(preciceDt, dt);
+            for (int j = 0; j < dimensions; j++) {
+                if ( readVectorData.at(j + dimensions * i) != writeVectorData.at(j + dimensions * i) ) {
+                    std::cout << "DUMMY (" << mpiHelper.rank()
+                              << "): Reading initialized VECTOR data error\n"
+                              << "Expected " << writeVectorData.at(j + dimensions * i)
+                              << ", Found " << readVectorData.at(j + dimensions * i)
+                              << "\n";
+                }
+            }
+        }
 
         ++iter;
 
+        for (int i = 0; i < numberOfVertices; i++) {
+            writeScalarData.at(i) = i + numberOfVertices + iter;
+            for (int j = 0; j < dimensions; j++) {
+                writeVectorData.at(j + dimensions * i) = i + iter;
+            }
+        }
+
+        // Write scalar data via DuMuX ID <-> preCICE ID mapping
+        for (int i = 0; i < numberOfVertices; i++) {
+            const double value = i + iter;
+            couplingInterface.writeScalarQuantityOnFace(writeScalarDataID,
+                                dumuxVertexIDs[i],
+                                value);
+        }
+        couplingInterface.writeScalarQuantityToOtherSolver(writeScalarDataID);
+
+        // Write vector data
+        couplingInterface.writeQuantityVector(writeVectorDataID, writeVectorData);
+        couplingInterface.writeScalarQuantityToOtherSolver(writeVectorDataID);
+
+        preciceDt = couplingInterface.advance(preciceDt);
+
         if (couplingInterface.hasToReadIterationCheckpoint()) {
-            //            //Read checkpoint
-            //            freeFlowVtkWriter.write(vtkTime);
-            //            vtkTime += 1.;
-            sol = sol_checkpoint;
-            freeFlowGridVariables->update(sol);
-            freeFlowGridVariables->advanceTimeStep();
-            //freeFlowGridVariables->init(sol);
+            std::cout << "DUMMY (" << mpiHelper.rank() << "): Reading iteration checkpoint\n";
             couplingInterface.announceIterationCheckpointRead();
-        } else  // coupling successful
+        } else
         {
-            // write vtk output
-            freeFlowVtkWriter.write(vtkTime);
+            std::cout << "DUMMY (" << mpiHelper.rank() << "): Advancing in time\n";
         }
     }
     ////////////////////////////////////////////////////////////
@@ -138,6 +211,7 @@ try {
     ////////////////////////////////////////////////////////////
 
     couplingInterface.finalize();
+      std::cout  << "DUMMY (" << mpiHelper.rank() << "): Closing C++ solver dummy...\n";
 
     // print dumux end message
     if (mpiHelper.rank() == 0) {
@@ -150,14 +224,6 @@ try {
 catch (Dumux::ParameterException &e) {
     std::cerr << std::endl << e << " ---> Abort!" << std::endl;
     return 1;
-} catch (Dune::DGFException &e) {
-    std::cerr << "DGF exception thrown (" << e
-              << "). Most likely, the DGF file name is wrong "
-                 "or the DGF file is corrupted, "
-                 "e.g. missing hash at end of file or wrong number "
-                 "(dimensions) of entries."
-              << " ---> Abort!" << std::endl;
-    return 2;
 } catch (Dune::Exception &e) {
     std::cerr << "Dune reported error: " << e << " ---> Abort!" << std::endl;
     return 3;
