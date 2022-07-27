8634075e35c297e27988ba78898ac11497e7dd99
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2022-07-27 17:32:31 +0200

    Add more modular Dockerfile

--- docker/checkout_repositories.sh
+++ docker/checkout_repositories.sh
@@ -1,19 +1,27 @@
-#/usr/bin/env sh
+#/usr/bin/env bash
 
 DUNEVERSION="${1}"
 DUMUXVERSION="${2}"
 
+if [[ $# -lt 2 ]]; then
+    echo "DUNE or DUMUX version missing"
+    exit 1
+fi
+
 echo "DUNE version ${DUNEVERSION}"
 echo "DUMUX version ${DUMUXVERSION}"
 
-for module in ('common', 'geometry', 'grid', 'localfunctions', 'istl', 'subgrid')
+for module in 'common' 'geometry' 'grid' 'localfunctions' 'istl' 'subgrid'
 do
     echo "Checking out module: ${module}"
-    if [[ module == 'subgrid']]; then
+    if [[ ${module} == 'subgrid' ]]; then
         git clone https://git.imp.fu-berlin.de/agnumpde/dune-${module}.git -b releases/$DUNEVERSION
     else
         git clone https://gitlab.dune-project.org/core/dune-${module}.git -b releases/$DUNEVERSION
     fi
 done
 
+echo "Checking out module: dumux"
 git clone https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git -b releases/$DUMUXVERSION
+
+ls
