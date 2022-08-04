12540ecc1d0e83adcdb4af07b995163b978276e6
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2022-08-04 20:44:22 +0200

    Action for Docker containers

--- docker/checkout_repositories.sh
+++ docker/checkout_repositories.sh
@@ -15,13 +15,13 @@ for module in 'common' 'geometry' 'grid' 'localfunctions' 'istl' 'subgrid'
 do
     echo "Checking out module: ${module}"
     if [[ ${module} == 'subgrid' ]]; then
-        git clone https://git.imp.fu-berlin.de/agnumpde/dune-${module}.git -b releases/$DUNEVERSION
+        git clone --depth 1 https://git.imp.fu-berlin.de/agnumpde/dune-${module}.git -b releases/$DUNEVERSION
     else
-        git clone https://gitlab.dune-project.org/core/dune-${module}.git -b releases/$DUNEVERSION
+        git clone --depth 1 https://gitlab.dune-project.org/core/dune-${module}.git -b releases/$DUNEVERSION
     fi
 done
 
 echo "Checking out module: dumux"
-git clone https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git -b releases/$DUMUXVERSION
+git clone --depth 1 https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git -b releases/$DUMUXVERSION
 
 ls
