4ecb2374640f3cc517c069bd0dfe1c0933184427
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2022-09-14 16:21:48 +0200

    Use Docker containers from preCICE namespace (#13)
    * Use Docker containers from preCICE namespace
    * Add build for `dune-precice` containers in GitHub Action
    * Remove last ajaust DockerHub namespace references

--- docker/rebuild-docker-images.sh
+++ docker/rebuild-docker-images.sh
@@ -7,14 +7,14 @@ PRECICE_VERSIONS=("2.4.0" "2.3.0")
 for dumux_version in ${DUMUX_VERSIONS[@]}; do
     for precice_version in ${PRECICE_VERSIONS[@]}; do
         echo "Building dumux-precice:${dumux_version}-${precice_version}"
-        docker build --build-arg DUNEVERSION=${DUNE_VERSION} --build-arg DUMUXVERSION=${dumux_version} --build-arg PRECICEVERSION=${precice_version} -t ajaust/dumux-precice:${dumux_version}-${precice_version} --file dockerfile.slim .
-        docker push ajaust/dumux-precice:${dumux_version}-${precice_version}
+        docker build --build-arg DUNEVERSION=${DUNE_VERSION} --build-arg DUMUXVERSION=${dumux_version} --build-arg PRECICEVERSION=${precice_version} -t precice/dumux-precice:${dumux_version}-${precice_version} --file dockerfile.slim .
+        docker push precice/dumux-precice:${dumux_version}-${precice_version}
         yes | docker image prune
     done
 done
 
 PRECICE_VERSION="2.4.0"
 echo "Building dune-precice:${dumux_version}-${precice_version}"
-docker build --build-arg DUNEVERSION=${DUNE_VERSION} --build-arg PRECICEVERSION=${PRECICE_VERSION} -t ajaust/dune-precice:${DUNE_VERSION}-${PRECICE_VERSION} --file dockerfile_dune-precice.slim .
-docker push ajaust/dune-precice:${DUNE_VERSION}-${PRECICE_VERSION}
+docker build --build-arg DUNEVERSION=${DUNE_VERSION} --build-arg PRECICEVERSION=${PRECICE_VERSION} -t precice/dune-precice:${DUNE_VERSION}-${PRECICE_VERSION} --file dockerfile_dune-precice.slim .
+docker push precice/dune-precice:${DUNE_VERSION}-${PRECICE_VERSION}
 yes | docker image prune
