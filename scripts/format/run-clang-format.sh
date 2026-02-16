94f44276389a1cac6df211e0de20337ccda7c0e2
Ishaan Desai <ishaandesai@gmail.com>
2026-02-16 17:46:09 +0100

    Remove postprocessing folder as it is no longer used (#69)
    * Remove postprocessing folder as it is no longer used
    
    * Remove postprocessing folder form clang format run

--- scripts/format/run-clang-format.sh
+++ scripts/format/run-clang-format.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 # Runs clang-format on all *.cc and *.hh files
-DIRECTORIES=("dumux-precice/" "examples/" "test/" "postprocessing/")
+DIRECTORIES=("dumux-precice/" "examples/" "test/")
 for DIRECTORY in "${DIRECTORIES[@]}"; do
     #echo "Check files in ${DIRECTORY}"
     for FILE in $(find "${DIRECTORY}" -regex '.*\.\(cc\|hh\)' ); do
